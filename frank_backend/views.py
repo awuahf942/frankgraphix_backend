1df4cf67834a4edc8570a63d3e43e556af94c2a7
awuahf942 <162237246+awuahf942@users.noreply.github.com>
2025-02-20 08:29:48 -0800

    Update views.py

--- frank_backend/views.py
+++ frank_backend/views.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 from rest_framework.response import Response
 
 class sendDetails(APIView):
-    def post(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         send_mail(
             "New Graphic Design Client Details",
             "Here is the test message, so it works if you receive it, it may probably be in your spam",
